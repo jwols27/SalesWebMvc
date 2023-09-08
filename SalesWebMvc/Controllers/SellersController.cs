72d3c7e4180761758cfadc35463035f23dccf31b
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-08 16:53:29 -0300

    Implemented Seller Create

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers {
     public class SellersController : Controller {
@@ -15,5 +16,17 @@ namespace SalesWebMvc.Controllers {
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
