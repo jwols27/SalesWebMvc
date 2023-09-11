f9c6afee37584a95c1b62f8fa6cb98ceab3b2b09
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-11 10:03:44 -0300

    Implemented sales simple search

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -41,6 +41,7 @@ namespace SalesWebMvc {
             services.AddScoped<SeedingService>();
             services.AddScoped<DepartmentService>();
             services.AddScoped<SellerService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
