f276d0c4c9755bde9815945b09d1dbdbc16ea47d
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-11 08:52:43 -0300

    Implemented Seller validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -32,6 +32,12 @@ namespace SalesWebMvc.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) {
+            if (!ModelState.IsValid) {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+                
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -81,6 +87,12 @@ namespace SalesWebMvc.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) {
+            if (!ModelState.IsValid) {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+                
             if (id != seller.Id)
                 return RedirectToAction(nameof(Error), new { message = "ID mismatch" });
 
