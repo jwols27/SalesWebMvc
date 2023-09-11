a3f9c5027bc9d70d99fde1bc6484570e7bb53648
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-11 13:41:38 -0300

    Finished Sales Record CRUD

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -2,8 +2,10 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -39,6 +41,83 @@ namespace SalesWebMvc.Controllers {
             await _salesRecordService.InsertAsync(salesRecord);
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null)
+                return RedirectToAction(nameof(Error), new { message = "ID not provided" });
+
+            var obj = await _salesRecordService.FindByIdAsync(id.Value);
+            if (obj == null)
+                return RedirectToAction(nameof(Error), new { message = "ID not found" });
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id) {
+            try {
+                await _salesRecordService.RemoveByIdAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e) {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+        }
+
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null)
+                return RedirectToAction(nameof(Error), new { message = "ID not provided" });
+
+            var obj = await _salesRecordService.FindByIdAsync(id.Value);
+            if (obj == null)
+                return RedirectToAction(nameof(Error), new { message = "ID not found" });
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Edit(int? id) {
+            if (id == null)
+                return RedirectToAction(nameof(Error), new { message = "ID not provided" });
+
+            var obj = await _salesRecordService.FindByIdAsync(id.Value);
+            if (obj == null)
+                return RedirectToAction(nameof(Error), new { message = "ID not found" });
+
+            List<Seller> sellers = await _sellerService.FindAllAsync();
+            SalesRecordFormViewModel viewModel = new SalesRecordFormViewModel { SalesRecord = obj, Sellers = sellers };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, SalesRecord salesRecord) {
+            if (!ModelState.IsValid) {
+                var sellers = await _sellerService.FindAllAsync();
+                var viewModel = new SalesRecordFormViewModel { SalesRecord = salesRecord, Sellers = sellers };
+                return View(viewModel);
+            }
+
+            if (id != salesRecord.Id)
+                return RedirectToAction(nameof(Error), new { message = "ID mismatch" });
+
+            try {
+                await _salesRecordService.UpdateAsync(salesRecord);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException e) {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+        }
+
+        public IActionResult Error(string message) {
+            var viewModel = new ErrorViewModel {
+                Message = message,
+                RequestId = Activity.Current?.Id
+            };
+            return View(viewModel);
+        }
+
 
         public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate) {
             if (!minDate.HasValue)
