508c04fae22d97a2db216cdd297eb057a49d3dee
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-11 12:00:01 -0300

    Migrated Updated SalesRecord

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -6,14 +6,20 @@ namespace SalesWebMvc.Models {
     public class SalesRecord {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.Currency)]
         public double Amount { get; set; }
         
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
+
+        [Display(Name = "Seller")]
+        public int SellerId { get; set; }
 
         public SalesRecord() { }
 
